2a23bd95230500ac128f40f9b8479f3537f2c5ad
treykris <trey10027@gmail.com>
2019-02-11 21:02:26 -0500

    Create DrumPad HTML Elements and listeners for those elements

--- src/DrumPad.ts
+++ src/DrumPad.ts
@@ -3,11 +3,13 @@ import Pad from './Pad';
 
 export default class DrumPad extends Pad{ 
 
+    
 
     constructor(songURL: string, keyboard:Keyboard, keyCode: number, audioContext: AudioContext){
         super(keyboard, keyCode, audioContext);
         this.songURL = songURL;
         this.fetchDefaultSample();  
+        this.buildPadHTML();
           
     }
 
@@ -17,7 +19,7 @@ export default class DrumPad extends Pad{
         .then(response =>  response.arrayBuffer())
         .then(arrayBuffer => this.audioContext.decodeAudioData(arrayBuffer))
         .then((audioBuffer) => {
-            this.audioBuffer = audioBuffer
+            this.setAudioBuffer(audioBuffer);
             this.establishAudioSource();
         })
         .catch((err: Error) => {
@@ -26,28 +28,154 @@ export default class DrumPad extends Pad{
     }
     
 
-    // load(audioFile: Element){
 
-    //     const fileReader = new FileReader();
+    buildPadHTML(){
+        const drumMachine = document.querySelector('.drum-machine')
 
-    //     fileReader.readAsArrayBuffer(audioFile.files[0]);
-    //     fileReader.onload = function(evt){
+        const article = document.createElement('article');
+        const drumPadContainer = document.createElement('div');
+        // const swapIcon = document.createElement('svg');
+        const swapIcon = document.createElement('span');
+        swapIcon.textContent = 'Swap Key'
+
+        const drumPad = document.createElement('div');
+        const loadBtnLabel = document.createElement('label');
+        const loadBtn = document.createElement('input');
+        const resetBtn = document.createElement('button');
+
+        const padControls = document.createElement('div');
+        const gainText = document.createElement('span');
+        const gainInput = document.createElement('input');
+        const pitchText = document.createElement('span');
+        const pitchInput = document.createElement('input');
+
+
+        loadBtnLabel.setAttribute('for', 'load-pad');
+        loadBtnLabel.textContent = 'Load';
+        loadBtn.type = 'file';
+        loadBtn.id = 'load-pad';
+        loadBtn.style.visibility = 'hidden';
         
-    //     audioBuffer = this.audioContext.decodeAudioData(fileReader.result);
+        resetBtn.textContent = 'Reset';
+        gainText.textContent = 'Gain';
+        pitchText.textContent = 'Pitch';
 
-        
-    //     audioBuffer.then((res) => {
+        drumPad.textContent = String.fromCharCode(this.keyCode);
 
-    //         this.audioBuffer = res;
+        this.padElement = drumPad;
+
+        this.padListener(drumPad);
+
+        this.loadListener(loadBtn);
+        this.resetListener(resetBtn);
+        this.swapListener(swapIcon);
+        this.setPadElementText();
+
+
+        article.classList.add('drum-pad');
+        drumPad.classList.add('drum-pad__pad');
+
+
+        drumMachine.appendChild(article);
+        article.appendChild(swapIcon);
+        article.appendChild(drumPadContainer);
+        article.appendChild(padControls);
+        drumPadContainer.appendChild(drumPad);
+        drumPadContainer.appendChild(loadBtnLabel);
+        drumPadContainer.appendChild(loadBtn);
+        drumPadContainer.appendChild(resetBtn);
+        padControls.appendChild(gainText);
+        padControls.appendChild(gainInput);
+        padControls.appendChild(pitchText);
+        padControls.appendChild(pitchInput);
+
+
+        // Mock Layout
+        // <article data-pad="9" class="drum-pad">
+        //     <span>Swap Key</span>
+        //     <div>
+        //         <div class="drum-pad__pad">
+        //             H
+        //         </div>
+        //         <span class="drum-pad__load">Load</span>
+        //         <span class="drum-pad__reset">Reset</span>
+        //     </div>
+        //     <div class="drum-pad__controls">
+        //         <span>Gain</span>
+        //         <button class="drum-pad__gain">{Gain volume knob}</button>
+                
+        //         <span>Pitch</span>
+        //         <button class="drum-pad__pitch">{Pitch volume knob}</button>
+        //     </div>
+        // </article>
+
+
+    }
+
+
+    padListener(elem: any){
+        elem.addEventListener('click', () => {
+            this.play();
+        })
+    }
+
+    gainListener(elem: Element){
+        elem.addEventListener('mousemove', (evt) => {
+            // this.gainInput = evt.target
+        })
+    }
+
+    pitchListener(elem: Element){
+        elem.addEventListener('mousemove', (evt) => {
+            // this.pitchInput = evt.target
+            // this.keyboard.captureWindowEvent(evt);
+        })
+    }
+
     
+
+    loadListener(elem: Element){
+        elem.addEventListener('input', (evt) => {
+            this.decodeBuffer(evt);
+        })
+    }
+
+    resetListener(elem: Element){
+        elem.addEventListener('click', (evt) => {
+            this.fetchDefaultSample();
+        })
+    }
+
+    swapListener(elem: Element){
+        elem.addEventListener('click', (evt) => {
+            console.log(evt);
+            this.keyboard.setReferencePad(this);
+            // this.setKeyCode(evt.target.value)
+        })
+    }
+
+    // Decodes an audio file when loaded
+    decodeBuffer(evt: Event){
+
+        const _this = this;
+
+        console.log(evt);
+
+        const fileReader = new FileReader();
+
+        fileReader.readAsArrayBuffer(evt.target.files[0]);
+        fileReader.onload = function(evt){
             
-    //     })
-    //     .catch((err: Error) => {
-    //         console.log(err);
-    //     })
-    // }
-
-
-
-
+    
+            let audioBuffer = _this.audioContext.decodeAudioData(fileReader.result as any);
+    
+            audioBuffer.then((res: AudioBuffer) => {
+                _this.setAudioBuffer(res);
+                _this.establishAudioSource();
+            })
+            .catch((err: Error) => {
+                console.log(err);
+            })
+        }
+    }
 }
