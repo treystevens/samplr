fd1a90b713dc66a75988acbea5e16e10fc5ced25
treykris <trey10027@gmail.com>
2019-02-14 17:07:50 -0500

    renamed to drumKitSamples

---
+++ src/samples/drumKitSamples.ts
@@ -0,0 +1,35 @@
+export const hipHopDrumKit: any = [
+    {
+        songURL: 'https://res.cloudinary.com/dr4eajzak/video/upload/v1549691476/Snare_178.wav',
+        charCode: 122
+    },
+    {
+        songURL: 'https://res.cloudinary.com/dr4eajzak/video/upload/v1549697588/Hat_67.wav',
+        charCode: 120
+    }, 
+    {
+        songURL: 'https://res.cloudinary.com/dr4eajzak/video/upload/v1549697609/Dilla_Kk_3.wav',
+        charCode: 99
+    },
+    {
+        songURL: 'https://res.cloudinary.com/dr4eajzak/video/upload/v1549691476/Snare_178.wav',
+        charCode: 118
+    },
+    {
+        songURL: 'https://res.cloudinary.com/dr4eajzak/video/upload/v1549697588/Hat_67.wav',
+        charCode: 98
+    }, 
+    {
+        songURL: 'https://res.cloudinary.com/dr4eajzak/video/upload/v1549697588/Hat_67.wav',
+        charCode: 110
+    }, 
+    {
+        songURL: 'https://res.cloudinary.com/dr4eajzak/video/upload/v1549697609/Dilla_Kk_3.wav',
+        charCode: 109
+    }, 
+    {
+        songURL: 'https://res.cloudinary.com/dr4eajzak/video/upload/v1549697609/Dilla_Kk_3.wav',
+        charCode: 44
+    }
+]
+
