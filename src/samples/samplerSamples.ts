9fddf404fe4f2feaf97e2ec3c1c8c1af74e19c28
treykris <trey10027@gmail.com>
2019-02-14 17:47:41 -0500

    Added electric piano sample bank

---
+++ src/samples/samplerSamples.ts
@@ -0,0 +1,70 @@
+export const electricPiano: any = [
+
+
+    {
+        // C
+        songURL: 'https://res.cloudinary.com/dr4eajzak/video/upload/v1550170579/samples/elec_piano/elecP_C.wav',
+        charCode: 97 // Keyboard Key: A
+    },
+    {
+        // C#
+        songURL: 'https://res.cloudinary.com/dr4eajzak/video/upload/v1550170582/samples/elec_piano/elecP_CSHARP.wav',
+        charCode: 119 // Keyboard Key: W
+    },
+    {
+        // D
+        songURL: 'https://res.cloudinary.com/dr4eajzak/video/upload/v1550170584/samples/elec_piano/elecP_D.wav',
+        charCode: 115 // Keyboard Key: S
+    },
+    {
+        // D#
+        songURL: 'https://res.cloudinary.com/dr4eajzak/video/upload/v1550170582/samples/elec_piano/elecP_DSHARP.wav',
+        charCode: 101 // Keyboard Key: E
+    },
+    {
+        // E
+        songURL: 'https://res.cloudinary.com/dr4eajzak/video/upload/v1550170583/samples/elec_piano/elecP_E.wav',
+        charCode: 100 // Keyboard Key: D
+    },
+    {
+        // F
+        songURL: 'https://res.cloudinary.com/dr4eajzak/video/upload/v1550170587/samples/elec_piano/elecP_F.wav',
+        charCode: 102 // Keyboard Key: F
+    },
+    {
+        // F#
+        songURL: 'https://res.cloudinary.com/dr4eajzak/video/upload/v1550170586/samples/elec_piano/elecP_FSHARP.wav',
+        charCode: 116 // Keyboard Key: T
+    },
+    {
+        // G
+        songURL: 'https://res.cloudinary.com/dr4eajzak/video/upload/v1550170588/samples/elec_piano/elecP_G.wav',
+        charCode: 103 // Keyboard Key: G
+    },
+    {
+        // G#
+        songURL: 'https://res.cloudinary.com/dr4eajzak/video/upload/v1550170588/samples/elec_piano/elecP_GSHARP.wav',
+        charCode: 121 // Keyboard Key: Y
+    },
+    {
+        // A
+        songURL: 'https://res.cloudinary.com/dr4eajzak/video/upload/v1550170578/samples/elec_piano/elecP_A.wav',
+        charCode: 104 // Keyboard Key: H
+    },
+    {
+        // A#
+        songURL: 'https://res.cloudinary.com/dr4eajzak/video/upload/v1550170583/samples/elec_piano/elecP_ASHARP.wav',
+        charCode: 117 // Keyboard Key: U
+    },
+    {
+        // B
+        songURL: 'https://res.cloudinary.com/dr4eajzak/video/upload/v1550170577/samples/elec_piano/elecP_B.wav',
+        charCode: 106 // Keyboard Key: J
+    },
+    {
+        // C4
+        songURL: 'https://res.cloudinary.com/dr4eajzak/video/upload/v1550170589/samples/elec_piano/elecP_C4.wav',
+        charCode: 107 // Keyboard Key: K
+    },
+
+];
