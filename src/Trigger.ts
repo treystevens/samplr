3a9b60ae0f143db72e71eb8078f1215f9125473a
treykris <trey10027@gmail.com>
2019-02-16 14:28:12 -0500

    Event Listeners and attributes for setting audio controls for triggers

--- src/Trigger.ts
+++ src/Trigger.ts
@@ -3,14 +3,16 @@ import { masterStreamNode, scriptNode } from './app';
 
 export let mediasource: any
 
+const wavesurfer: any = WaveSurfer.create({
+    container: '#waveform',
+    waveColor: 'orange',
+    progressColor: 'purple'
+});
 
 export default abstract class Trigger{
 
     key: string
     audioBuffer: AudioBuffer
-    gainInput: number
-    filterKnob: AudioNode
-    pitchInput: number
     audioContext: AudioContext
     audioSource: AudioBufferSourceNode
     defaultSampleURL: string
@@ -18,17 +20,168 @@ export default abstract class Trigger{
     triggerElement: HTMLElement
     gainElementInput: HTMLInputElement
     pitchElementInput: HTMLInputElement
+    qElementInput: HTMLInputElement
+    frequencyElementInput: HTMLInputElement
     gainSlider: HTMLInputElement
     pitchSlider: HTMLInputElement
+    frequencySlider: HTMLInputElement
+    qSlider: HTMLInputElement
+    pitch: number
+    gain: number
+    active: boolean
+    filterOption: string
+    frequency: number
+    q: number
+    filterOptionList: Array<HTMLInputElement>
+    userSelectedFilterOption: HTMLElement
 
 
     constructor(audioContext: AudioContext, sampler: Sampler, key: string){
         this.sampler = sampler;
         this.key = key;
         this.audioContext = audioContext;
-        this.gainInput = 1;
-        this.pitchInput = 0;
-        this.filterKnob = audioContext.createBiquadFilter();
+        this.pitch = 0;
+        this.gain = 0;
+        this.q = 0;
+        this.frequency = 20000;
+        this.filterOption = 'lowpass';
+        this.active = false;
+
+        this.initAudioControlSelectors();
+        this.initAudioControlListeners();    
+    }
+
+    setActive(active: boolean){
+        this.active = active;
+        
+        if(this.active){
+
+            this.pitchSlider.valueAsNumber = this.pitch;
+            this.pitchElementInput.value = String(this.pitch);
+            this.gainSlider.valueAsNumber = this.gain;
+            this.gainElementInput.value = String(this.gain);
+
+            this.frequencySlider.valueAsNumber = this.frequency;
+            this.frequencyElementInput.value = String(this.frequency);
+            this.qSlider.valueAsNumber = this.q;
+            this.qElementInput.value = String(this.q);
+
+
+            this.filterOptionList.forEach((option) => {
+
+                if(option.value !== this.filterOption){
+                    option.checked = false;
+                }
+                else{
+                    option.checked = true;
+                }   
+            });
+        }
+    }
+
+    initAudioControlSelectors(): void{
+        // Gain Elements
+        this.gainElementInput = document.querySelector('.sampler__gain-text');
+        this.gainSlider = document.querySelector('.sampler__gain-slider');
+        
+        // Pitch Elements
+        this.pitchElementInput = document.querySelector('.sampler__pitch-text');
+        this.pitchSlider = document.querySelector('.sampler__pitch-slider');
+
+        // Filter Elements
+        // -- Frequency elements
+        this.frequencyElementInput = document.querySelector('.sampler__frequency-text');
+        this.frequencySlider = document.querySelector('.sampler__frequency-slider');
+
+        // -- Q elements
+        this.qElementInput = document.querySelector('.sampler__q-text');
+        this.qSlider = document.querySelector('.sampler__q-slider');
+
+        this.filterOptionList = Array.from(document.querySelectorAll('.sampler__filter-option'))
+    }
+
+    initAudioControlListeners(){
+        // Gain Input Listeners
+        this.gainSlider.addEventListener('input', (evt) => {
+            if(this.active){
+                this.gain = evt.target.value;
+                this.gainElementInput.value = String(this.gain);
+            }
+        })
+        this.gainElementInput.addEventListener('input', (evt) => {
+            if(this.active){
+             const value = Number(evt.target.value)
+             
+             this.gain = value;
+             if(value > 6) this.gain = 6;
+             if (value < -70) this.gain = -70;
+              
+             this.gainSlider.valueAsNumber = this.gain;
+            } 
+        })
+
+        // Pitch Input Listeners
+        this.pitchSlider.addEventListener('input', (evt) => {
+            if(this.active){
+                this.pitch = evt.target.value;
+                this.pitchElementInput.value = String(this.pitch);
+            }
+        })
+        this.pitchElementInput.addEventListener('input', (evt) => {
+            const value = Number(evt.target.value)
+
+            this.pitch = value;
+            if(value > 12) this.pitch= 12;
+            if (value < -12) this.pitch= -12;
+
+            this.pitchSlider.valueAsNumber = this.pitch;
+            
+        })
+
+        // Filter Input Listeners
+        this.frequencySlider.addEventListener('input', (evt) => {
+            if(this.active){
+                this.frequency = evt.target.value;
+                this.frequencyElementInput.value = String(this.frequency);
+            }
+        })
+        this.frequencyElementInput.addEventListener('input', (evt) => {
+            const value = Number(evt.target.value)
+
+            this.frequency = value;
+            if(value > 20000) this.frequency= 20000;
+            if (value < 26) this.frequency= 26;
+
+            this.frequencySlider.valueAsNumber = this.frequency;
+            
+        })
+        this.qSlider.addEventListener('input', (evt) => {
+            if(this.active){
+                this.q = evt.target.value;
+                this.qElementInput.value = String(this.q);
+            }
+        })
+        this.qElementInput.addEventListener('input', (evt) => {
+            const value = Number(evt.target.value)
+
+            this.q = value;
+            if(value > 16) this.q = 16;
+            if (value < 0) this.q = 0;
+
+            this.qSlider.valueAsNumber = this.q;
+            
+        })
+        this.filterOptionList.forEach((option) => {
+            if(option.value === 'lowpass') this.userSelectedFilterOption = option;
+
+            option.addEventListener('input', (evt) => {
+                if(this.active){
+                    this.userSelectedFilterOption.checked = false;
+                    this.filterOption = evt.target.value;
+                    this.userSelectedFilterOption = option;
+                }
+            })
+        })
     }
 
     getKey(): string{
@@ -51,48 +204,40 @@ export default abstract class Trigger{
         this.audioBuffer = audioBuffer;
     }
 
-
-
     establishAudioSource(){
 
         const gainNode = this.audioContext.createGain();
-        const gainLevel = Math.pow(10, this.gainInput / 20);
-
+        const gainLevel = Math.pow(10, this.gain / 20);
+        const biquadFilter = this.audioContext.createBiquadFilter();
+        
         this.audioSource = this.audioContext.createBufferSource();
         this.audioSource.buffer = this.audioBuffer;
 
-        // console.log(this.audioSource)
-        // mediasource = this.audioContext.createMediaElementSource(this.audioSource);
-
         gainNode.gain.value = gainLevel;
+        biquadFilter.type = this.filterOption;
+        biquadFilter.frequency.value = this.frequency;
+        biquadFilter.Q.value = this.q;
 
-        // +100 and -100 detune the source up or down by one semitone, 
-        // while +1200 and -1200 detune it up or down by one octave.
-        // pitchInput is range [-12, 12] (inclusive);
-        // ORIGINAL ~~~~~ BELOW the 3 BELOW
-        // this.audioSource.detune.value = this.pitchInput * 100;
-        // this.audioSource.connect(gainNode);
-        // gainNode.connect(this.audioContext.destination);
-
-
-
-        this.audioSource.detune.value = this.pitchInput * 100;
+        // MDN: +100 and -100 detune the source up or down by one semitone, 
+        // MDN: while +1200 and -1200 detune it up or down by one octave.
+        // this.pitch is range [-12, 12];
+        this.audioSource.detune.value = this.pitch * 100;
         this.audioSource.connect(gainNode);
-        // scriptNode.connect(gainNode);
-        gainNode.connect(this.audioContext.destination);
-
-
         
+        gainNode.connect(biquadFilter);
+        biquadFilter.connect(this.audioContext.destination)
+
         // The code right below works for MediaRecorder
-        gainNode.connect(masterStreamNode);
-
-        
-        // masterStreamNode.connect(this.audioContext.destination);
+        biquadFilter.connect(masterStreamNode);
     }
 
     play(){
         this.establishAudioSource();
+        // Show wave form when playing
+        wavesurfer.load(this.defaultSampleURL);
         this.audioSource.start();
+        
+        
     }
 
     setTriggerElementText(){
@@ -107,22 +252,47 @@ export default abstract class Trigger{
         }
     }
 
-    setGainElementInputText(){
-        this.gainElementInput.value = String(this.gainInput);
+    // Decodes an audio file when loaded
+    decodeBuffer(evt: Event){
+
+        const _this = this;
+
+        const fileReader = new FileReader();
+
+        fileReader.readAsArrayBuffer(evt.target.files[0]);
+        fileReader.onload = function(){
+            
+            const audioBuffer = _this.audioContext.decodeAudioData(fileReader.result as any);
+    
+            audioBuffer.then((res: AudioBuffer) => {
+                _this.setAudioBuffer(res);
+                _this.establishAudioSource();
+            })
+            .catch((err: Error) => {
+                console.log(err);
+            })
+        }
     }
 
-    setPitchElementInputText(){
-        this.pitchElementInput.value = String(this.pitchInput);
+    fetchDefaultSample(): void{
+
+        fetch(this.defaultSampleURL)
+        .then(response =>  response.arrayBuffer())
+        .then(arrayBuffer => this.audioContext.decodeAudioData(arrayBuffer))
+        .then((audioBuffer) => {
+            this.setAudioBuffer(audioBuffer);
+            this.establishAudioSource();
+        })
+        .catch((err: Error) => {
+            console.log(err);
+        })
     }
 
-    setGainSlider(){
-        this.gainSlider.value = this.gainElementInput.value;
+    triggerListener(elem: any){
+        elem.addEventListener('click', () => {
+            this.play();
+        })
     }
-
-    setPitchSlider(){
-        this.pitchSlider.value = this.pitchElementInput.value;
-    }
-
 
     abstract buildHTML(): void
 }
