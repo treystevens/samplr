ec5f262293e3cbfd014284c29dc732a860c46938
treykris <trey10027@gmail.com>
2019-02-16 14:30:07 -0500

    Changed Keyboard to Sampler, added buildHTML function

--- src/Key.ts
+++ src/Key.ts
@@ -1,12 +1,47 @@
-import Keyboard from './Keyboard';
 import Trigger from './Trigger';
-
+import Sampler from './Sampler';
 
 export default class Key extends Trigger{ 
 
-
-    constructor(keyboard: Keyboard, charCode: number, audioContext: AudioContext){
-        super(keyboard, charCode, audioContext);          
+    constructor(audioContext: AudioContext, sampler: Sampler, key: string, defaultSampleURL?: string,){
+        super(audioContext, sampler, key);
+        this.defaultSampleURL = defaultSampleURL;
+        this.fetchDefaultSample();  
+        this.buildHTML();    
     }
+  
+    buildHTML(){
+       
+        const keysContainer = document.querySelector('.keys-container');
+        const key = document.createElement('div');        
+        let keyClass;
+        switch (this.key) {
+            case 'w':
+                keyClass = 'key__black';
+                break;
+            case 'e':
+                keyClass = 'key__black';
+                break;
+            case 't':
+                keyClass = 'key__black';
+                break;
+            case 'y':
+                keyClass = 'key__black';
+                break;
+            case 'u':
+                keyClass = 'key__black';
+                break;
+            default:
+                keyClass = 'key__white'
+                break;
+        }
 
+        this.triggerElement = key;
+        this.setTriggerElementText();
+        this.triggerListener(key);
+
+        key.classList.add(keyClass);
+
+        keysContainer.appendChild(key);
+    }
 }
