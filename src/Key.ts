cf64e0d85b175e4ea0586b30c3d77c545f9177db
treykris <trey10027@gmail.com>
2019-02-20 23:29:57 -0500

    Show musical note with associated key

--- src/Key.ts
+++ src/Key.ts
@@ -1,19 +1,38 @@
 import Trigger from './Trigger';
 import Sampler from './Sampler';
 
+enum musicNote{
+    a = 'C',
+    w = 'C#',
+    s = 'D',
+    e = 'D#',
+    d = 'E',
+    f = 'F',
+    t = 'F#',
+    g = 'G',
+    y = 'G#',
+    h = 'A',
+    u = 'A#',
+    j = 'B',
+    k = 'C',
+
+}
+
 export default class Key extends Trigger{ 
 
-    constructor(audioContext: AudioContext, sampler: Sampler, key: string, defaultSampleURL?: string,){
+    constructor(audioContext: AudioContext, sampler: Sampler, key: string, sampleURL?: string,){
         super(audioContext, sampler, key);
-        this.defaultSampleURL = defaultSampleURL;
-        this.fetchDefaultSample();  
+        this.sampleURL = sampleURL;
+        this.fetchSample();
         this.buildHTML();    
     }
   
     buildHTML(){
        
         const keysContainer = document.querySelector('.keys-container');
-        const key = document.createElement('div');        
+        const key = document.createElement('div');  
+        const keyboardKey = document.createElement('span');
+        const musicKey = document.createElement('span');      
         let keyClass;
         switch (this.key) {
             case 'w':
@@ -36,12 +55,23 @@ export default class Key extends Trigger{
                 break;
         }
 
+        if (!window.matchMedia('screen and (max-width: 820px)').matches) {
+            keyboardKey.textContent = String(this.getKey().toUpperCase();
+        }
+
         this.triggerElement = key;
-        this.setTriggerElementText();
+
         this.triggerListener(key);
+        
+        musicKey.textContent = musicNote[this.getKey()];
 
         key.classList.add(keyClass);
+        key.classList.add('key')
+        keyboardKey.classList.add('key__keyboard-note')
+        musicKey.classList.add('key__music-note')
 
         keysContainer.appendChild(key);
+        key.appendChild(keyboardKey);
+        key.appendChild(musicKey);
     }
 }
